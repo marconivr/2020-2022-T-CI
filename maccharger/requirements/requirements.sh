13c01448ce40149c2161b453a293b64bbbd4e14d
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-03-31 16:31:01 +0200

    Removed all

--- maccharger/requirements/requirements.sh
+++
@@ -1,8 +0,0 @@
-echo Installer for linux by Castellani Davide >> ./../log/log.log
-
-sudo apt-get install net-tools -y >> ./../log/log.log
-sudo apt-get install python3 -y >> ./../log/log.log
-sudo apt-get install python3-pip -y >> ./../log/log.log
-sudo pip3 install -r ./requirements.txt >> ./../log/log.log
-
-echo >> ./../log/log.log
